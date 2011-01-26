095ea863cf7be532bfeb60ab93e351111dbdb189
dgerstin <dgerstin@ca2c5cd7-39d7-495c-9a21-ea95cd046ca5>
2011-01-26 20:10:13 +0000

    Deleting the old PA database files. These will be replaced with the new ones.

--- PA/admin/logoffForm.php
+++
@@ -1,9 +0,0 @@
-<form method="post" action="index.php">
-	<?php
-		print "<input type=\"hidden\" name=\"username\" value=";
-		print $_SESSION['username'];
-		print ">";
-	?>
-
-	<input class="button" type="submit" name="submit" value="Logoff">
-</form>
