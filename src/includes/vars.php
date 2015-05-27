751e7c2f8cbf572531eb711ca5335d8015d59ff5
Michael Bond <mbond@the-forgotten.org>
2015-05-27 15:05:45 -0400

    moved local var definition to vars.php

--- src/includes/vars.php
+++ src/includes/vars.php
@@ -2,6 +2,7 @@
 
 $localvars = localvars::getInstance();
 
+$localvars->set('pageTitle',"WVU Libraries: Databases");
 $localvars->set("currentDisplayObjectTitle","Databases A-Z List");
 $localvars->set('dbConnectionName', 'appDB');
 $localvars->set("databaseHome","/databases");
