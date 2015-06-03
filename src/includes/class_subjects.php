bbd46bba84119df3fdcba506d8f997ffd67c37df
Michael Bond <mbond@the-forgotten.org>
2015-06-03 09:50:57 -0400

    get all the subjects, or an individual subject

--- src/includes/class_subjects.php
+++ src/includes/class_subjects.php
@@ -2,6 +2,32 @@
 
 class subjects {
 	
+	// if $ID is null, return all, otherwise, return ID
+	// returns array
+	public static function get($ID=NULL) {
+
+		$localvars = localvars::getInstance();
+		$db       = db::get($localvars->get('dbConnectionName'));
+
+		if (!isnull($ID) && validate::integer($ID)) {
+			$whereClause = sprintf("WHERE `ID`=%s", $ID);
+		}
+		else {
+			$whereClause = "";
+		}
+
+		$sql       = sprintf("SELECT * FROM `subjects` %s ORDER BY `name`", $whereClause);
+		$sqlResult = $db->query($sql);
+
+		$subjects = array();
+		while ($row = $sqlResult->fetch()) {
+			$subjects[$row['name']] = array("ID" => $row['ID'], "URL" => $row['url']);
+		}
+
+		return $subjects;
+
+	}
+
 }
 
 ?>
