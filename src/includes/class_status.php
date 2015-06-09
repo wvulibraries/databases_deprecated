7ccc0f64daab3a23fa49e483d6d344d8d94beb0a
Michael Bond <mbond@the-forgotten.org>
2015-06-09 07:11:34 -0400

    Build the SQL status clause

--- src/includes/class_status.php
+++ src/includes/class_status.php
@@ -26,6 +26,36 @@ class status {
 
 	}
 
+	public static function buildSQLStatus() {
+
+		$status    = self::current();
+		$localvars = localvars::getInstance();
+
+		switch($localvars->get('status')) {
+			case 1:
+			case "published":
+				$status = "dbList.status='1'";
+				break;
+			case 2:
+			case "development":
+				$status = "dbList.status='1' OR dbList.status='2'";
+				break;
+			case 3:
+			case "hidden":
+				$status = "dbList.status='3'";
+				break;
+			case 4:
+			case "all":
+				$status = "dbList.status='1' OR dbList.status='2' OR dbList.status='3'";
+				break;
+			default:
+				$status = "";
+		}
+
+		return $status;
+
+	}
+
 	public static function build() {
 
 		$status = self::current();
