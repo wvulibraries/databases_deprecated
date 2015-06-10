4b69379e0fc5154a10d0f614e039f30d881b650b
Michael Bond <mbond@the-forgotten.org>
2015-06-10 17:27:37 -0400

    get a resource type

--- src/includes/class_resourceTypes.php
+++ src/includes/class_resourceTypes.php
@@ -1,6 +1,32 @@
 <?php
 
 class resourceTypes {
+
+	public static function get($ID=NULL) {
+
+		$localvars = localvars::getInstance();
+		$db        = db::get($localvars->get('dbConnectionName'));
+		$validate  = validate::getInstance();
+
+		if (!isnull($ID) && $validate->integer($ID)) {
+			$whereClause = sprintf("WHERE `ID`=%s", $ID);
+		}
+		else {
+			$whereClause = "";
+		}
+
+		$sql       = sprintf("SELECT * FROM `resourceTypes` %s ORDER BY `name`", $whereClause);
+		$sqlResult = $db->query($sql);
+
+		if ($sqlResult->error()) {
+			errorHandle::newError($sqlResult->errorMsg(), errorHandle::DEBUG);
+			return FALSE;
+		}
+
+		return $sqlResult->fetchAll();
+
+	}
+
 	
 }
 
