269cbcd8d9d3a972a47b8d9dd0f60031c4fc8ab9
Michael Bond <mbond@the-forgotten.org>
2015-06-10 15:28:16 -0400

    Updated.

--- src/type/index.php
+++ src/type/index.php
@@ -1,24 +1,7 @@
 <?php
+require "../engineHeader.php";
 
-require_once("/home/library/public_html/includes/engineHeader.php");
-
-$engine->localVars('pageTitle',"WVU Libraries: Databases");
-
-// $engine->eTemplate("load","library2012.2col.right");
-$engine->eTemplate("load","library2014-backpage");
-
-recurseInsert("dbTables.php","php");
-require_once("/home/library/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php");
-$engineVars['openDB'] = $engine->dbConnect("database","databases",FALSE);
-
-// Fire up the Engine
-$engine->eTemplate("include","header");
-?>
-
-<?php
-include("buildStatus.php");
-
-switch($engine->cleanGet['HTML']['type']) {
+switch($_GET['HTML']['type']) {
 	case "new":
 	$pageHeader = "New";
 	$searchType = "newDatabase";
@@ -44,36 +27,24 @@ switch($engine->cleanGet['HTML']['type']) {
 	$searchType = "fullTextDB";
 }
 
+$localvars->set("pageHeader",$pageHeader);
+
+$dbObject  = new databases;
+$databases = $dbObject->getByType($searchType);
+$localvars->set("databases",lists::databases($databases));
+
+templates::display('header'); 
 ?>
 
-<?php
-
-recurseInsert("buildLists.php","php");
-
-?>
 
 <!-- Page Content Goes Below This Line -->
 
 <div class="clearfix" id="subjectsContainer">
 
-<h3><?php print (!empty($pageHeader))?$pageHeader:"Error"; ?> Databases</h3>
+<h3>{local var="pageHeader"} Databases</h3>
 
 
-<?php
-
-$sql = "select * from dbList WHERE ".$searchType."='1' AND (".$status.") ORDER BY name";
-$engineVars['openDB']->sanitize = FALSE;
-$sqlResult = $engineVars['openDB']->query($sql);
-
-if (!$sqlResult['result']) {
-	// print webHelper_errorMsg("SQL Error: ".$sqlResult['error']);
-	print webHelper_errorMsg("Error Retrieving database");
-}
-?>
-
-<?php
-	include("buildDBListing.php");
-?>
+{local var="databases"}
 
 </div>
 
@@ -87,5 +58,5 @@ if (!$sqlResult['result']) {
 <!-- Page Content Goes Above This Line -->
 
 <?php
-$engine->eTemplate("include","footer");
+templates::display('footer'); 
 ?>
