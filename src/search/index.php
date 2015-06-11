a738cb5f948a8f77441c24e6f2d205135bf72dcb
Michael Bond <mbond@the-forgotten.org>
2015-06-10 20:28:40 -0400

    updated.

--- src/search/index.php
+++ src/search/index.php
@@ -1,54 +1,17 @@
 <?php
+require "../engineHeader.php";
 
-require_once("/home/library/public_html/includes/engineHeader.php");
+$_GET['MYSQL']['q'] = ($_GET['MYSQL']['q'] && !is_empty($_GET['MYSQL']['q']))?$_GET['MYSQL']['q']:"";
+$_GET['MYSQL']['q'] = urldecode($_GET['MYSQL']['q']);
+$_GET['MYSQL']['q'] = str_replace("&", "&amp;", $_GET['MYSQL']['q']);
 
-require "../includes/databases.php";
+$dbObject  = new databases;
+$databases = $dbObject->find($_GET['MYSQL']['q']);
+$localvars->set("databases",lists::databases($databases));
+$localvars->set("displayNoResults",(is_empty($databases))?"block":"none");
+$localvars->set("query",$_GET['HTML']['q']);
 
-$engine->localVars('pageTitle',"WVU Libraries: Databases");
-// $engine->eTemplate("load","library2012.2col.right");
-$engine->eTemplate("load","library2014-backpage");
-
-recurseInsert("dbTables.php","php");
-require("/home/library/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php");
-$engineVars['openDB'] = $engine->dbConnect("database","databases",FALSE);
-
-recurseInsert("buildLists.php","php");
-
-// Check for and expire trial databases
-$sql = "UPDATE dbList set status='2' WHERE trialDatabase=1 AND trialExpireDate<".time();
-$engineVars['openDB']->sanitize = FALSE;
-$sqlResult = $engineVars['openDB']->query($sql);
-//
-
-$localVars['status'] = 1;
-if(!empty($engine->cleanGet['HTML']['status']) && isint($engine->cleanGet['HTML']['status'])) {
-	$localVars['status'] = $engine->cleanGet['HTML']['status'];
-}
-
-$engine->cleanGet['MYSQL']['q'] = urldecode($engine->cleanGet['MYSQL']['q']);
-$engine->cleanGet['MYSQL']['q'] = str_replace("&", "&amp;", $engine->cleanGet['MYSQL']['q']);
-
-$sqlResult = array();
-if ($engine->cleanGet['MYSQL']['q'] && !isempty($engine->cleanGet['MYSQL']['q'])) {
-	$sql       = sprintf('SELECT * FROM `dbList` WHERE `name` LIKE "%%%s%%"',
-		$engine->cleanGet['MYSQL']['q']
-	);
-	$sqlResult = $engineVars['openDB']->query($sql);
-
-}
-
-$result = databases::buildDBListing($sqlResult);
-
-if (is_empty($result)) {
-	$result = sprintf('<br /><br />We do not have any databases named "<strong>%s</strong>".  Please browse our <a href="%s">Databases</a> by subject or title.',
-		$engine->cleanGet['HTML']['q'],
-		"/databases"
-		);
-}
-
-// Fire up the Engine
-$engine->eTemplate("include","header");
-
+templates::display('header'); 
 ?>
 
 <!-- Page Content Goes Below This Line -->
@@ -57,16 +20,15 @@ $engine->eTemplate("include","header");
 
 <h3>Database Search Results</h3>
 
-<?php print $result ?>
-</div>
+<p style="display: {local var="displayNoResults"}">We do not have any databases named "<strong>{local var="query"}</strong>".  Please browse our <a href="{local var="databaseHome"}">Databases</a> by subject or title.</p>
 
-<div id="rightNav">
-
-<?php recurseInsert("rightNav.php","php"); ?>
+{local var="databases"}
 
 </div>
+
+
 <!-- Page Content Goes Above This Line -->
 
 <?php
-$engine->eTemplate("include","footer");
+templates::display('footer'); 
 ?>
