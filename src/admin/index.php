5b5c1b5b78abb8a064bcc627c936d867ac64d620
Michael Bond <mbond@the-forgotten.org>
2015-06-15 13:40:25 -0400

    Updated for Engine 4.0

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,19 +1,7 @@
 <?php
+require "../engineHeader.php";
 
-$engineDir = "/home/library/phpincludes/engineAPI/engine";
-include($engineDir ."/engine.php");
-$engine = new EngineCMS();
-
-$engine->localVars('pageTitle',"Database Management");
-
-recurseInsert("dbTables.php","php");
-require_once("/home/library/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php");
-$engine->dbConnect("database","databases",TRUE);
-
-recurseInsert("acl.php","php");
-$engine->accessControl("build");
-
-$engine->eTemplate("include","header");
+templates::display('header');
 ?>
 
 <!-- Page Content Goes Below This Line -->
@@ -22,5 +10,5 @@ $engine->eTemplate("include","header");
 <!-- Page Content Goes Above This Line -->
 
 <?php
-$engine->eTemplate("include","footer");
+templates::display('footer');
 ?>
