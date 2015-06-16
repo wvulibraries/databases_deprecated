c21a8ad6b3dc3827b55cfc2955fecdd6ba1659fb
Michael Bond <mbond@the-forgotten.org>
2015-06-16 13:12:38 -0400

    Updated VEndors

---
+++ src/admin/includes/forms/vendors.php
@@ -0,0 +1,36 @@
+<?php
+
+$form = formBuilder::createForm('Vendors');
+$form->linkToDatabase(array(
+    'table'       => 'vendors'
+));
+
+$form->insertTitle = "New Vendor";
+$form->editTitle   = "Edit Vendors";
+
+
+$form->addField(
+    array(
+        'name'            => 'ID',
+        'primary'         => TRUE,
+        'showIn'          => array(formBuilder::TYPE_INSERT, formBuilder::TYPE_UPDATE),
+        'type'            => 'hidden'
+    )
+);
+
+$form->addField(
+    array(
+        'name'  => 'name',
+        'label' => 'Vendor'
+    )
+);
+
+$form->addField(
+    array(
+        'name'  => 'url',
+        'label' => 'Vendor URL',
+        'type'  => 'url'
+    )
+);
+
+?>
--- src/admin/management/vendors/index.php
+++ src/admin/management/vendors/index.php
@@ -1,76 +1,24 @@
 <?php
-$engineDir = "/home/library/phpincludes/engineAPI/engine";
-include($engineDir ."/engine.php");
-$engine = new EngineCMS();
+require_once "../../../engineHeader.php";
 
-$engine->localVars('pageTitle',"Database Management: Vendors");
+recurseInsert("includes/forms/vendors.php","php");
 
-recurseInsert("dbTables.php","php");
-require_once("/home/library/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php");
-$engine->dbConnect("database","databases",TRUE);
-
-recurseInsert("acl.php","php");
-$engine->accessControl("build");
-
-$engine->eTemplate("include","header");
+templates::display('header');
 ?>
 
-<?php
-$dbTables = $engine->dbTablesExport();
+<header>
+<h1>Vendor Management</h1>
+</header>
 
-$engine->localVars('listAddLabel', "Vendors");
-$engine->localVars('listAddTable', $dbTables["vendors"]["prod"]);
+<section>
+{form name="Vendors" display="form"}
+</section>
 
-$localVars = $engine->localVarsExport();
+<section>
+{form name="Vendors" display="editTable"}
+</section>
 
-$cols = array();
-$cols[1]["table"] = "name";
-$cols[1]["label"] = "Vendor";
-
-$cols[2]["table"] = "url";
-$cols[2]["label"] = "URL";
-
-?>
-
-
-<!-- Page Content Goes Below This Line -->
-
-<h2>{local var="pageTitle"}</h2>
 
 <?php
-//Submit the form
-
-if(isset($engine->cleanPost['MYSQL']['newSubmit'])) {
-	
-	$output = webHelper_listMultiInsert($localVars['listAddTable'],$localVars['listAddLabel'],$cols,$engine);
-
-	echo $output;
-
-}
-else if (isset($engine->cleanPost['MYSQL']['updateSubmit'])) {
-	$output = webhelper_listMultiUpdate($localVars['listAddTable'],$cols,$engine);
-	
-	echo $output;
-}
-
-?>
-
-<h3>{local var="listAddLabel"} Management</h3>
-
-{engine name="function" function="webHelper_listMultiAdd" table="{local var="listAddTable"}" label="{local var="listAddLabel"}" cols="2" <?php 
-foreach ($cols as $I=>$col) {
-	echo sprintf("col%s=\"%s\" col%slabel=\"%s\" ",$I,$col["table"],$I,$col["label"]);
-}
-?>}
-
-
-
-<hr />
-
-{engine name="function" function="webHelper_listMultiEditList" table="{local var="listAddTable"}" cols="2" col1="<?php print $cols[1]["table"]; ?>" col2="<?php print $cols[2]["table"]; ?>" col1label="<?php print $cols[1]["label"]; ?>" col2label="<?php print $cols[2]["label"]; ?>"}
-
-<!-- Page Content Goes Above This Line -->
-
-<?php
-$engine->eTemplate("include","footer");
+templates::display('footer');
 ?>
