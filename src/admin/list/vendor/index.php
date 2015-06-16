b23e2f04a4c85b949e20d552fca1e1a96811ff99
Michael Bond <mbond@the-forgotten.org>
2015-06-16 08:44:17 -0400

    Moved directories around

--- src/admin/list/vendor/index.php
+++ src/admin/list/vendor/index.php
@@ -1,9 +1,10 @@
 <?php
+
 $engineDir = "/home/library/phpincludes/engineAPI/engine";
 include($engineDir ."/engine.php");
 $engine = new EngineCMS();
 
-$engine->localVars('pageTitle',"Database Management: Vendors");
+$engine->localVars('pageTitle',"Database Management: Add Database");
 
 recurseInsert("dbTables.php","php");
 require_once("/home/library/phpincludes/databaseConnectors/database.lib.wvu.edu.remote.php");
@@ -15,59 +16,117 @@ $engine->accessControl("build");
 $engine->eTemplate("include","header");
 ?>
 
-<?php
-$dbTables = $engine->dbTablesExport();
-
-$engine->localVars('listAddLabel', "Vendors");
-$engine->localVars('listAddTable', $dbTables["vendors"]["prod"]);
-
-$localVars = $engine->localVarsExport();
-
-$cols = array();
-$cols[1]["table"] = "name";
-$cols[1]["label"] = "Vendor";
-
-$cols[2]["table"] = "url";
-$cols[2]["label"] = "URL";
-
-?>
-
-
 <!-- Page Content Goes Below This Line -->
 
-<h2>{local var="pageTitle"}</h2>
-
 <?php
-//Submit the form
 
-if(isset($engine->cleanPost['MYSQL']['newSubmit'])) {
-	
-	$output = webHelper_listMultiInsert($localVars['listAddTable'],$localVars['listAddLabel'],$cols,$engine);
+// Check for and expire trial databases
+$sql = "UPDATE dbList set status='2' WHERE trialDatabase=1 AND trialExpireDate<".time();
+$engine->openDB->sanitize = FALSE;
+$sqlResult = $engine->openDB->query($sql);
+//
 
-	echo $output;
+$error = FALSE;
 
-}
-else if (isset($engine->cleanPost['MYSQL']['updateSubmit'])) {
-	$output = webhelper_listMultiUpdate($localVars['listAddTable'],$cols,$engine);
-	
-	echo $output;
+$sql = "SELECT dbList.ID as ID, dbList.name as name, vendors.name as vendorName, dbList.status as status, dbList.trialExpireDate as trialExpireDate, dbList.trialDatabase as trialDatabase, vendors.url as vendorURL FROM dbList LEFT JOIN vendors on dbList.vendor=vendors.ID ORDER BY vendors.name,dbList.name";
+
+$engine->openDB->sanitize = FALSE;
+$sqlResult = $engine->openDB->query($sql);
+
+if (!$sqlResult['result']) {
+	$error = TRUE;
+	print webHelper_errorMsg("SQL Error:".$sqlResult['error']."<br /> SQL:".$sql);
 }
 
 ?>
 
-<h3>{local var="listAddLabel"} Management</h3>
+<?php if ($error == FALSE) { ?>
 
-{engine name="function" function="webHelper_listMultiAdd" table="{local var="listAddTable"}" label="{local var="listAddLabel"}" cols="2" <?php 
-foreach ($cols as $I=>$col) {
-	echo sprintf("col%s=\"%s\" col%slabel=\"%s\" ",$I,$col["table"],$I,$col["label"]);
+	<table border="0" cellpadding="1" cellspacing="0">
+		<tr style="background-color: #EEEEFF;">
+			<th style="width: 50px; text-align: left;">
+				ID
+			</th>
+			<th style="width: 300px; text-align: left;">
+				Vendor
+			</th>
+			<th style="width: 300px; text-align: left;">
+				Title
+			</th>
+			<th style="width: 120px; text-align: left;">
+				Status
+			</th>
+			<th style="width: 150px; text-align: left;">
+				Trial
+			</th>
+		</tr>
+		<tr><td colspan="5" style="background-color: #000000;"></td></tr>
+		
+<?php		
+$count = 0;
+while ($row = mysql_fetch_array($sqlResult['result'], MYSQL_ASSOC)) {
+
+$color = (++$count%2 == 0)?$engineVars['oddColor']:$engineVars['evenColor'];
+
+
+?>
+
+<tr style="background-color: <?php print $color; ?>;">
+	<td>
+		<a href="newdatabase.php?id=<?php print $row['ID']; ?>">Edit</a>
+	</td>
+	<td>
+		<?php if (!is_empty($row['vendorURL'])) { ?>
+			<a href="<?php print $row['vendorURL']; ?> ">
+		<?}?>
+		
+		<?php print (is_empty($row['vendorName']))?"none":$row['vendorName'];  ?>
+		
+		<?php if (!is_empty($row['vendorURL'])) { ?>
+			</a>
+		<?}?>
+	</td>
+	<td>
+		<?php print $row['name']; ?>
+	</td>
+	<td>
+		<?php
+		
+	        switch($row['status']) {
+				case 1:
+				echo '<span style="color:green;">Published</span>';
+				break;
+				case 2:
+				echo '<span style="color:#D9E455;">Development</span>';
+				break;
+				case 3:
+				echo '<span style="color:red;">Hidden</span>';
+				break;
+				default:
+				echo '<span style="color:red;font-weight: bold">ERROR</span>';
+			}
+				
+?>
+	</td>
+	<td>
+		<?php
+	    if($row['trialDatabase'] == 1) {
+			print "Yes -- ".date("M d, Y",$row['trialExpireDate']);
+		}
+		else {
+			echo "No";
+		} 
+		?>
+	</td>
+</tr>
+
+<?php		
 }
-?>}
+?>
+		
+	</table>
 
-
-
-<hr />
-
-{engine name="function" function="webHelper_listMultiEditList" table="{local var="listAddTable"}" cols="2" col1="<?php print $cols[1]["table"]; ?>" col2="<?php print $cols[2]["table"]; ?>" col1label="<?php print $cols[1]["label"]; ?>" col2label="<?php print $cols[2]["label"]; ?>"}
+<?php } ?>
 
 <!-- Page Content Goes Above This Line -->
 
