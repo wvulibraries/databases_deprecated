a88fdf87cebaa9256727857ac938035d8407695d
Michael Bond <mbond@the-forgotten.org>
2015-06-16 13:12:24 -0400

    moved and updated subjects

---
+++ src/admin/includes/forms/subjects.php
@@ -0,0 +1,36 @@
+<?php
+
+$form = formBuilder::createForm('Subjects');
+$form->linkToDatabase(array(
+    'table'       => 'subjects'
+));
+
+$form->insertTitle = "New Subject";
+$form->editTitle   = "Edit Subjcets";
+
+
+$form->addField(
+    array(
+        'name'            => 'ID',
+        'primary'         => TRUE,
+        'showIn'          => array(formBuilder::TYPE_INSERT, formBuilder::TYPE_UPDATE),
+        'type'            => 'hidden'
+    )
+);
+
+$form->addField(
+    array(
+        'name'  => 'name',
+        'label' => 'Subject'
+    )
+);
+
+$form->addField(
+    array(
+        'name'  => 'url',
+        'label' => 'Subject URL',
+        'type'  => 'url'
+    )
+);
+
+?>
---
+++ src/admin/management/subjects/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once "../../../engineHeader.php";
+
+recurseInsert("includes/forms/subjects.php","php");
+
+templates::display('header');
+?>
+
+<header>
+<h1>Subject Management</h1>
+</header>
+
+<section>
+{form name="Subjects" display="form"}
+</section>
+
+<section>
+{form name="Subjects" display="editTable"}
+</section>
+
+
+<?php
+templates::display('footer');
+?>
