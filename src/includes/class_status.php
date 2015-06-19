da99701723d744b5a21135187f0fe43bd3a84daa
Michael Bond <mbond@the-forgotten.org>
2015-06-19 13:01:30 -0400

    switched to validator class

--- src/includes/class_status.php
+++ src/includes/class_status.php
@@ -5,8 +5,9 @@ class status {
 	public static function set() {
 
 		$localvars = localvars::getInstance();
+		$validate  = validate::getInstance();
 
-		if(isset($_GET['HTML']['status']) && !is_empty($_GET['HTML']['status']) && isint($_GET['HTML']['status'])) {
+		if(isset($_GET['HTML']['status']) && !is_empty($_GET['HTML']['status']) && $validate->integer($_GET['HTML']['status'])) {
 			$localvars->set("status",$_GET['HTML']['status']);
 		}
 		else {
