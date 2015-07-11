2f3afca1f1bbb52cc2b4dd0c21083231afb3bd6c
Michael Bond <mbond@the-forgotten.org>
2015-07-11 06:23:03 -0400

    commented out old header includes

--- src/headerIncludes.php
+++ src/headerIncludes.php
@@ -1,2 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <link rel="stylesheet" href="/css/2014/webapps.css" type="text/css" media="screen" />
+ -->
