af420fb7c3daef118f4549f08831d9fc7b2f9ee1
Michael Bond <mbond@the-forgotten.org>
2015-07-11 11:53:54 -0400

    All of the "types" of databases put into an Array

--- src/includes/vars.php
+++ src/includes/vars.php
@@ -9,4 +9,11 @@ $localvars->set("databaseHome","/databases");
 $localvars->set("connectURL","/databases/connect.php");
 $localvars->set("proxyURL", "http://www.libproxy.wvu.edu/login?url=");
 $localvars->set("descriptionLength","300");
+$localvars->set("databaseTagTypes",array(
+	// "fullTextDB"    => "Full Text",
+	"newDatabase"   => "New", 
+	"trialDatabase" => "Trial", 
+	"alumni"        => "Alumni", 
+	"mobile"        => "Mobile"
+	));
 ?>
