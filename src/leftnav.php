421189756708bcd68b01286e9fa246d316185089
Michael Bond <mbond@the-forgotten.org>
2015-07-13 13:21:01 -0400

    fixed the html structure/semantics
    nested ULs have to be inside the same LI as the parent.

--- src/leftnav.php
+++ src/leftnav.php
@@ -23,22 +23,22 @@ $localvars->set("popular",lists::popular($popularDatabases));
 	<div id="facets">
 		<h2>Narrow Your Results</h2>
 		<ul>
-			<li class="facets-header">Types of Databases
-				<span class="facetToggle">+</span>
 			<li>
 				<span class="facets-header">Start Here / Top Picks<span class="facetToggle">+</span></span>
 				{local var="popular"}
 			</li>
-			<ul>
-				<li><a href="{local var="databaseHome"}/type/alumni/">Alumni</a><i class="fa fa-angle-right"></i></li>
-				<li><a href="{local var="databaseHome"}/type/new/">New</a><i class="fa fa-angle-right"></i></li>
-				<li><a href="{local var="databaseHome"}/type/trial/">Trial</a><i class="fa fa-angle-right"></i></li>
-			</ul>
-			<li class="facets-header">Resource Types
-				<span class="facetToggle">+</span>
+			<li><span class="facets-header">Types of Databases<span class="facetToggle">+</span></span>
+
+				<ul>
+					<li><a href="{local var="databaseHome"}/type/alumni/">Alumni</a><i class="fa fa-angle-right"></i></li>
+					<li><a href="{local var="databaseHome"}/type/new/">New</a><i class="fa fa-angle-right"></i></li>
+					<li><a href="{local var="databaseHome"}/type/trial/">Trial</a><i class="fa fa-angle-right"></i></li>
+				</ul>
 			</li>
+			<li><span class="facets-header">Resource Types<span class="facetToggle">+</span></span>
+
+				{local var="resourceTypes"}
 			</li>
-			{local var="resourceTypes"}
 		</ul>
 	</div>
 </div>
