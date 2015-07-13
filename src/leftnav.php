e2d1dc63c11f3a695bc780fc848c505dd6cfea89
Michael Bond <mbond@the-forgotten.org>
2015-07-13 13:19:59 -0400

    added popular databases to the leftnav

--- src/leftnav.php
+++ src/leftnav.php
@@ -5,6 +5,18 @@ $localvars->set("currentStatus",status::build());
 
 $localvars->set("resourceTypes",lists::resourceTypes());
 
+// Figure out which popular databases we need.
+$dbObject = new databases;
+if (1 == 2) {
+	$popularDatabases = 1;
+}
+else {
+	$popularDatabases = $dbObject->getByType("popular");
+}
+
+$localvars->set("popular",lists::popular($popularDatabases));
+
+
 ?>
 
 <div id="sidebar">
@@ -13,6 +25,9 @@ $localvars->set("resourceTypes",lists::resourceTypes());
 		<ul>
 			<li class="facets-header">Types of Databases
 				<span class="facetToggle">+</span>
+			<li>
+				<span class="facets-header">Start Here / Top Picks<span class="facetToggle">+</span></span>
+				{local var="popular"}
 			</li>
 			<ul>
 				<li><a href="{local var="databaseHome"}/type/alumni/">Alumni</a><i class="fa fa-angle-right"></i></li>
@@ -21,6 +36,7 @@ $localvars->set("resourceTypes",lists::resourceTypes());
 			</ul>
 			<li class="facets-header">Resource Types
 				<span class="facetToggle">+</span>
+			</li>
 			</li>
 			{local var="resourceTypes"}
 		</ul>
