a31224b247353ed68fc505401037f249beb49455
Michael Bond <mbond@the-forgotten.org>
2015-07-14 07:37:49 -0400

    Get the "top pick" databases for a subject.

--- src/includes/class_topPickDBs.php
+++ src/includes/class_topPickDBs.php
@@ -2,6 +2,27 @@
 
 class topPickDBs {
 	
+	public static function getTopPicksForSubject($id) {
+		
+		$localvars = localvars::getInstance();
+
+		$db        = db::get($localvars->get('dbConnectionName'));
+		$sql       = "SELECT `dbID` FROM `databases_curated` WHERE subjectID=?";
+		$sqlResult = $db->query($sql,array($id));
+
+		$databases = array();
+		$dbObject  = new databases;
+
+		while($row = $sqlResult->fetch()) {
+
+			$databases[] = $dbObject->get($row['dbID']);
+
+		}
+
+		return $databases;
+
+	}	
+
 }
 
 ?>
