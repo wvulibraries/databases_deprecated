137bbff500d0003630672d23607e3b0e843568e5
Michael Bond <mbond@the-forgotten.org>
2015-07-16 13:38:17 -0400

    removed debugging.

--- src/headerIncludes.php
+++ src/headerIncludes.php
@@ -2,10 +2,6 @@
 
 $localvars = localvars::getInstance();
 
-print "<pre>";
-var_dump($_SERVER['REQUEST_URI']);
-print "</pre>";
-
 if (!preg_match("/\/databases\/?(index.php)?$/",$_SERVER['REQUEST_URI'])) {
 	$localvars->set("enableBreadcrumbClicking","breadcrumbClicking");
 }
