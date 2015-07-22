880a6e02e44b0f1b053ce0a8e6bc2dc2d0cc7cc0
Michael Bond <mbond@the-forgotten.org>
2015-07-22 09:12:30 -0400

    don't show pagination on subjects list page

--- src/includes/pagination.php
+++ src/includes/pagination.php
@@ -1,1 +1,3 @@
+<?php if ($_SERVER['SCRIPT_NAME'] != "/databases/index.php") { ?>
 <span class="pagingPrevious" data-previous="">« Previous</span> | <span class="database-content-paging-pages"><span class="beginningResult">x</span> - <span class="endResult">y</span> of <span class="totalResults">z</span></span> | <span class="pagingNext" data-next="">Next »</span>
+<?php } ?>
