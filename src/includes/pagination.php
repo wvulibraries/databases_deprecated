6a45da806d4e3015b8382b8e5e8d6a6c6619f9af
Michael Bond <mbond@the-forgotten.org>
2015-07-23 07:05:27 -0400

    hide the pagination on the admin page.

--- src/includes/pagination.php
+++ src/includes/pagination.php
@@ -2,11 +2,12 @@
 $localvars = localvars::getInstance();
 if (!in_array($_SERVER['SCRIPT_NAME'], $localvars->get("noPagination"))) { 
 ?>
-
+<div class="pagingContainer">
 <div style="clear:both;"></div>
 <hr />
 <div style="clear:both;"></div>
 <div class="database-content-paging right">
 	<span class="pagingPrevious" data-previous="">« Previous</span> | <span class="database-content-paging-pages"><span class="beginningResult">x</span> - <span class="endResult">y</span> of <span class="totalResults">z</span></span> | <span class="pagingNext" data-next="">Next »</span>
+</div>
 </div>
 <?php } ?>
