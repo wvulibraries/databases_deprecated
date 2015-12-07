ba28a12f6fba079ea27105a4180a089728cf5d69
Michael Bond <mbond@the-forgotten.org>
2015-12-07 07:47:27 -0500

    Update the link for scifinder scholar account creation with proxy uri

--- src/scifinder/rightnav.php
+++ src/scifinder/rightnav.php
@@ -5,7 +5,7 @@
 						</a>
 					</li>
 					<li>
-						<a href="https://scifinder.cas.org/registration/index.html?corpKey=92E01248-86F3-F00A-118B-5F2F72172EFA">
+						<a href="http://www.libproxy.wvu.edu/login?url=https://scifinder.cas.org/registration/index.html?corpKey=92E01248-86F3-F00A-118B-5F2F72172EFA">
 							Create a SciFinder account
 						</a>
 					</li>
