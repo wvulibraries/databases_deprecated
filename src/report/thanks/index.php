64d56a367290bc8709456c468b42276749cc450c
Michael Bond <mbond@the-forgotten.org>
2016-02-19 07:47:23 -0500

    report form.

---
+++ src/report/thanks/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require "../../engineHeader.php";
+
+templates::display('header'); 
+
+?>
+
+<!-- Page Content Goes Below This Line -->
+
+{local var="letters"}
+
+<h3>eResource Error Report -- Thanks</h3>
+
+<p>
+Thank you for submitting a problem.
+</p>
+
+<!-- Page Content Goes Above This Line -->
+
+<?php
+templates::display('footer'); 
+?>
