af15cb422a8df618f8840a72006ea4532a90cf82
Michael Bond <mbond@the-forgotten.org>
2016-02-20 12:22:05 -0500

    bookmark

--- src/includes/class_lists.php
+++ src/includes/class_lists.php
@@ -141,6 +141,7 @@ class lists {
 					date("M d, Y",$database['trialExpireDate'])
 					);
 			}
+			$output .= '<span class="bookmark-false">Bookmark</span>';
 			$output .= '</div>'; // database-box-top
 
 			$output .= '<div class="database-box-bottom database-res">';
