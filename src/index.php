779acd4b3dcf28965842f5809cba6f8ab402a081
Michael Bond <mbond@the-forgotten.org>
2016-03-09 08:04:58 -0500

    Removed duplicate content and include via include.

--- src/index.php
+++ src/index.php
@@ -83,46 +83,7 @@ recurseInsert("stylesheets/homepage.css");
 <div style="clear:both;"></div>
 
 <div class="hphelp">
-    <h2>Help with Databases</h2>
-    <p>
-        Most databases are available off-campus to current WVU-Morgantown students, faculty and
-        staff. Licensing restrictions prevent WVU Libraries from providing off-campus access to
-        others. Your affiliation with WVU is verified through use of WVU Login. You will be
-        prompted to enter this when you choose a database from the menu. For assistance with
-        WVU Login, please go to the Information Technology Services (ITS) website at:
-        <a href="https://login.wvu.edu/">https://login.wvu.edu/</a>. There you can activate your
-        account or change your password. Alternatively, you may call the ITS Help Desk at
-        <span class="phoneNumber">(304) 293-4444</span>. If you need assistance with database
-        selection or searching please contact:
-    </p>
-    <div class="hphelp-messagebox">
-        <p><a href="mailto:Barbara.Hengemihle@mail.wvu.edu">Barbara Hengemihle</a><br />
-        Evansdale Library<br />
-        <span class="phoneNumber">(304) 293-9748</span><br /><br />
-        Evansdale Library Reference Desk<br />
-        <span class="phoneNumber">(304) 293-4695</span></p>
-    </div>
-    <div class="hphelp-messagebox">
-        <p><a href="mailto:Susan.Arnold@mail.wvu.edu">Susan Arnold</a><br />
-        Health Sciences Library<br />
-        <span class="phoneNumber">(304) 293-2105</span><br /><br />
-        Health Sciences Library Reference Desk<br />
-        <span class="phoneNumber">(304) 293-6810</span></p>
-    </div>
-    <div class="hphelp-messagebox">
-        <p><a href="mailto:ppugh@wvu.edu">Penny Pugh</a><br />
-            Downtown Campus Library<br />
-            <span class="phoneNumber">(304) 293-0334</span><br /><br />
-            Downtown Campus Library Reference Desk<br />
-            <span class="phoneNumber">(304) 293-3640</span>
-        </p>
-    </div>
-    <div>
-        <p><a href="http://westvirginia.libanswers.com/">Ask a Librarian</a><br />
-            <em>Chat/Text/Email Reference Service</em><br><br>
-            <em>If you are having a problem with a database or online resource, please submit an <a href="/databases/report/">eResource Error Report</a>.</em></p>
-            <!-- <br><br><strong>For technical problems call</strong> the WVU Library Systems Office at <span class="phoneNumber">(304) 293-0340</span> Monday-Friday, 8 a.m. to 4:00 p.m. or send e-mail to <a href="mailto:libsys@mail.wvu.edu">libsys@mail.wvu.edu</a>. -->
-    </div>
+    <?php recurseInsert("includes/database_help.php","php") ?>
 </div>
 
 <?php
