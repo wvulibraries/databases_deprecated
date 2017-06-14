818b3a600f644e7adac58f5305c6a4424aa33f6d
ddavisgraphics <dave@ddavisgraphics.com>
2017-06-14 08:53:59 -0400

    Fixing header using template for accessibility error

--- src/feedback/thanks/index.php
+++ src/feedback/thanks/index.php
@@ -2,15 +2,15 @@
 
 require "../../engineHeader.php";
 
-templates::display('header');
+$localvars->set("databaseHeading","eResource Trail Database Feedback");
+$localvars->set("databaseHeading","-- Thanks");
 
+templates::display('header');
 ?>
 
 <!-- Page Content Goes Below This Line -->
 
 {local var="letters"}
-
-<h2>eResource Trail Database Feedback -- Thanks</h2>
 
 <p>
 Thank you for submitting feedback about a trial database.
